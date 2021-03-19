5edd3ce93ba0081cc76caee2b036aa57ee11707f
Guizuka <51218840+Guizuka@users.noreply.github.com>
2021-03-19 00:45:09 -0300

    Lab9 done

---
+++ Assets/Data/SceneDataScriptableObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneData", menuName = "Data/SceneData")]
+
+public class SceneDataScriptableObject : ScriptableObject   
+{
+
+    //Player data
+    
+    [Header("Player Data")]
+    public Vector3 playerPosition;
+    public int playerHealth;
+
+
+}
